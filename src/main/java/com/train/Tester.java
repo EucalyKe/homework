2a65b61cf8fb42819778c1f059ccc7c46a0281c8
eucal <eucaly1026@gmail.com>
2019-04-18 19:13:21 +0800

    新增邏輯運算

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter number of tickets:");
-        int totaltickets = scanner.nextInt();
-        System.out.println("How many round-trip tickets:");
-        int roundtrip = scanner.nextInt();
-        Ticket t = new Ticket(totaltickets, roundtrip);
-        t.print();
+        int i = 0;
+        while (i > -1) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter number of tickets:");
+            int totaltickets = scanner.nextInt();
+            System.out.println("How many round-trip tickets:");
+            int roundtrip = scanner.nextInt();
+            if(totaltickets > roundtrip) {
+                Ticket t = new Ticket(totaltickets, roundtrip);
+                t.print();
+            } else {
+                System.out.println("Please enter again.");
+            }
+        }
+
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,13 +3,21 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    val scanner = Scanner(System.`in`)
-    print("Please enter number of tickets:")
-    var totaltickets = scanner.nextInt()
-    print("How many round-trip tickets:")
-    var roundtrip = scanner.nextInt()
-    val t = TicketKotlin(totaltickets, roundtrip)
-    t.print()
+    val i = 0
+    while (i > -1) {
+        val scanner = Scanner(System.`in`)
+        print("Please enter number of tickets:")
+        var totaltickets = scanner.nextInt()
+        print("How many round-trip tickets:")
+        var roundtrip = scanner.nextInt()
+        if(totaltickets > roundtrip) {
+            val t = TicketKotlin(totaltickets, roundtrip)
+            t.print()
+        } else {
+            println("Please enter again.");
+        }
+    }
+
 }
 
 class TicketKotlin(var totalticket : Int, var roundtrip : Int) {
