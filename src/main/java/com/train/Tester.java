9cc619f82acd82297ec0ba9da1a2f8eb0ac60840
eucal <eucaly1026@gmail.com>
2019-04-22 17:48:30 +0800

    Homework2 輸入-1終止購票系統

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,20 +4,32 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        int i = 0;
-        while (i > -1) {
+
+        int totaltickets = 0;
+        int roundtrip = 0;
+
+        while ((totaltickets != -1) && (roundtrip != -1)) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please enter number of tickets:");
-            int totaltickets = scanner.nextInt();
-            System.out.println("How many round-trip tickets:");
-            int roundtrip = scanner.nextInt();
-            if(totaltickets > roundtrip) {
-                Ticket t = new Ticket(totaltickets, roundtrip);
-                t.print();
+            totaltickets = scanner.nextInt();
+
+            if(totaltickets != -1) {
+                System.out.println("How many round-trip tickets:");
+                roundtrip = scanner.nextInt();
+
+                if(roundtrip != -1) {
+                    if(totaltickets >= roundtrip) {
+                        Ticket t = new Ticket(totaltickets, roundtrip);
+                        t.print();
+                    } else {
+                        System.out.println("Please enter again.");
+                    }
+                } else {
+                    System.out.println("The system is end.");
+                }
             } else {
-                System.out.println("Please enter again.");
+                System.out.println("The system is end.");
             }
         }
-
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,19 +3,27 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    val i = 0
-    while (i > -1) {
-        val scanner = Scanner(System.`in`)
-        print("Please enter number of tickets:")
-        var totaltickets = scanner.nextInt()
-        print("How many round-trip tickets:")
-        var roundtrip = scanner.nextInt()
-        if(totaltickets > roundtrip) {
-            val t = TicketKotlin(totaltickets, roundtrip)
-            t.print()
-        } else {
-            println("Please enter again.");
-        }
+    var totaltickets = 0
+    var roundtrip = 0
+    while ((totaltickets != -1) && (roundtrip != -1)) {
+        //val scanner = Scanner(System.`in`)
+        print("Please enter number of tickets:") //輸入購票張數
+        totaltickets = readLine()!!.toInt()
+        if(totaltickets != -1) { //判斷是否輸入-1,如是則結束系統
+            print("How many round-trip tickets:")
+            roundtrip = readLine()!!.toInt()
+
+            if(roundtrip != -1) { //判斷是否輸入-1,如是則結束系統
+                if(totaltickets >= roundtrip) {
+                    val t = TicketKotlin(totaltickets, roundtrip)
+                    t.print()
+                } else {
+                    println("Please enter again.");
+                }
+            } else println("The system is end.")
+
+        } else println("The system is end.")
+
     }
 
 }
