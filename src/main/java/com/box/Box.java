693f378b1b11321abcaa393f3485b442c74c182c
eucal <eucaly1026@gmail.com>
2019-05-01 16:27:41 +0800

    Homework3 郵局便利箱 將Box改成父類別

--- src/main/java/com/box/Box.java
+++ src/main/java/com/box/Box.java
@@ -1,5 +1,16 @@
 package com.box;
 
-public interface Box {
-    public boolean validate(float length, float width, int height);
+public class Box {
+    public float length;
+    public float width;
+    public float height;
+    public boolean validate(float length, float width, int height) {
+        if((length > this.length)
+                || (width > this.width)
+                || (height > this.height)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
